bc112cedd13a3bc0e7ed5441c3b64783046e408f
JuanGutiierrez <joselitoguti2003@gmail.com>
2023-11-11 16:11:25 -0500

    feat: diseñamos el primer filtro para la data de ventas, creamos archivo HTML con los filtros diseñados para ventas

--- helpers/crearCSV.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-def crearCSV(lista, nombreCSV):
-     with open('data/' +nombreCSV, mode='w', newline='', encoding='utf-8') as archivo_csv:
-          writer=csv.writer(archivo_csv)
-          writer.writerow(['NumeroOrden', 'Cliente', 'Costo'])
-          writer.writerows(lista)
-          
