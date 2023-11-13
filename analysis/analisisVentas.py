1fa44839136a407c7d82f8d3c01c0482f244fe51
JuanGutiierrez <joselitoguti2003@gmail.com>
2023-11-13 02:44:38 -0500

    feat: adicionamos el segundo filtro para analisisVentas

--- analysis/analisisVentas.py
+++ analysis/analisisVentas.py
@@ -9,11 +9,17 @@ from helpers.crearArchivosPDF import pdfGraficar
 crearCSV(ventas, 'ventas.csv')
 
 ventasDataFrame=pd.read_csv('data/ventas.csv')
+crearTabla(ventasDataFrame,'tablaventas')
 pdfGraficar(ventasDataFrame, "data/ventasTabla.pdf")
+
+ventasAltas = ventasDataFrame.nlargest(8, "Costo")
+imgGraficar(ventasAltas, "assets/img/graficas/ventasAltas.png","NumeroOrden","Costo","Numero de orden","Costo","Ventas mas altas en el ultimo año")
 
 ventasMayores=ventasDataFrame.query('Costo >= 600000')
 filtroUnoVentas=ventasMayores[['NumeroOrden','Costo']]
 crearTabla(filtroUnoVentas,'ventas600')
-imgGraficar(filtroUnoVentas, "assets/img/graficas/ventas600.png","NumeroOrden","Costo","Numero de orden","Costo","Ventas mayores a 600mil")
 
+ventasIntermedio = ventasDataFrame.query("(Costo>200000) and (Costo<600000)")
+filtroDosVentas = ventasIntermedio[['NumeroOrden', 'Costo']]
+crearTabla(filtroDosVentas,'ventasintermedio')
 
