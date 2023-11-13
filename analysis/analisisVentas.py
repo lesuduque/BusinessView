7e3b3f3c2278bee94a45d5b249e1865facf4914c
JuanGutiierrez <joselitoguti2003@gmail.com>
2023-11-13 02:20:40 -0500

    feat: diseñamos la funcion para generar las graficas tanto en formato PNG como en formato PDF

--- analysis/analisisVentas.py
+++ analysis/analisisVentas.py
@@ -4,14 +4,16 @@ from helpers.crearVentasCSV import crearCSV
 from helpers.crearTablaHTML import crearTabla
 from data.ventas import ventas
 from helpers.crearGraficas import imgGraficar
+from helpers.crearArchivosPDF import pdfGraficar
 
 crearCSV(ventas, 'ventas.csv')
 
 ventasDataFrame=pd.read_csv('data/ventas.csv')
+pdfGraficar(ventasDataFrame, "data/ventasTabla.pdf")
 
 ventasMayores=ventasDataFrame.query('Costo >= 600000')
 filtroUnoVentas=ventasMayores[['NumeroOrden','Costo']]
 crearTabla(filtroUnoVentas,'ventas600')
 imgGraficar(filtroUnoVentas, "assets/img/graficas/ventas600.png","NumeroOrden","Costo","Numero de orden","Costo","Ventas mayores a 600mil")
-imgGraficar(filtroUnoVentas, "data/ventas600.pdf","NumeroOrden","Costo","Numero de orden","Costo","Ventas mayores a 600mil")
+
 
