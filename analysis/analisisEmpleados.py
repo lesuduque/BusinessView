697d89a60c71946e538a24fefc6ab5d39c95da05
JuanGutiierrez <joselitoguti2003@gmail.com>
2023-11-13 02:58:00 -0500

    feat: creamos las tablas para analisisProductos y analisisEmpleados

--- analysis/analisisEmpleados.py
+++ analysis/analisisEmpleados.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
 from helpers.crearCSVempleados import crearCSV
+from helpers.crearTablaHTML import crearTabla
 from data.empleados import empleados
 
 crearCSV(empleados, 'empleados.csv')
 
 empleadosDataFrame=pd.read_csv('data/empleados.csv')
-print(empleadosDataFrame)
+crearTabla(empleadosDataFrame,'tablaempleados')
--- analysis/analisisProductos.py
+++ analysis/analisisProductos.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
 from helpers.crearCSVProductos import crearCSV
+from helpers.crearTablaHTML import crearTabla
 from data.productos import productos
 
 crearCSV(productos, 'productos.csv')
 
 productosDataFrame=pd.read_csv('data/productos.csv')
-print(productosDataFrame)
+crearTabla(productosDataFrame,'tablaproductos')
